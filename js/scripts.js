090bea2c13a7034d0227020b0507aa82531cd70e
John Miguel Dizon <jdizon11@my.bcit.ca>
2023-03-21 14:03:25 -0700

    added js and prompt script

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const courseList = {code: "ACIT 1620", name: "Web Fundamental Technologies"};
+
+let response
+
+do{ response = prompt("Enter 4-digit course code")
+
+}while(isNaN(response) == false)
+
