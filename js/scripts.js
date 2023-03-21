03966cfc02bf01768c6ef2fd7966b20afcbf1b31
John Miguel Dizon <jdizon11@my.bcit.ca>
2023-03-21 14:42:03 -0700

    added 3 items to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
-const courseList = {code: "ACIT 1620", name: "Web Fundamental Technologies"};
+const courseList = [{code: "ACIT 1620", name: "Web Fundamental Technologies"},
+                    {code:"ORGB 1100", name:"Organizational Behaviour"},
+                    {code:"ACIT 1630", name:"Database Management Systems"}];
 
-let response
+var response
+
+
+
 
 do{ response = prompt("Enter 4-digit course code")
 
-}while(isNaN(response) == false)
+}while(isNaN(response) == true)
+
+
+for (let i = 0; i < courseList.length; i++){
+    if(courseList.includes(response)){
+        console.log("YES")
+    }
+}
+
+
+// for (let i = 0; i < courseList.length; i++){
+//     if(courseList.includes(response)){
+//         console.log("Yes I am taking the course")
+//     }
+// }
 
