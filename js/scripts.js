d0861ccd4023bf283b548fdc47a68270c8f923a2
John Miguel Dizon <jdizon11@my.bcit.ca>
2023-03-21 21:55:57 -0700

    added confirm message

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,29 @@
-const courseList = [{code: "ACIT 1620", name: "Web Fundamental Technologies"},
-                    {code:"ORGB 1100", name:"Organizational Behaviour"},
-                    {code:"ACIT 1630", name:"Database Management Systems"}];
-
-var response
-
+let c1 = {code: "ACIT 1620", name: "Web Fundamental Technologies"};
+let c2 = {code:"ORGB 1100", name:"Organizational Behaviour"};
+let c3 = {code:"ACIT 1630", name:"Database Management Systems"};
+const courseList = [c1,c2,c3]
 
 
 
-do{ response = prompt("Enter 4-digit course code")
+
+let response
+
+do{ response = prompt("What is your first name?") 
 
 }while(isNaN(response) == true)
 
 
 for (let i = 0; i < courseList.length; i++){
-    if(courseList.includes(response)){
-        console.log("YES")
+    if(courseList[i].code.includes(response)){
+        console.log(`yes I am taking the course: ${courseList[i].name}`);
+        break;
     }
+    else{
+        courseList.push(response);
+    }
+
 }
+
 
 
 // for (let i = 0; i < courseList.length; i++){
